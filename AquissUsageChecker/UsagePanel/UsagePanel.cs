cccf12296c4bc67b228591f9ceff081cd8eba0e7
Dan Clarke <boogleoogle@gmail.com>
2012-09-10 22:04:40 +0100

    Fix shadow issue in usage panel

--- AquissUsageChecker/UsagePanel/UsagePanel.cs
+++ AquissUsageChecker/UsagePanel/UsagePanel.cs
@@ -1,38 +1,36 @@
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using MonoMac.Foundation;
 using MonoMac.AppKit;
-
 using AquissUsageChecker.StatusPanel;
 
 namespace AquissUsageChecker.UsagePanel
 {
-    public partial class UsagePanel : Panel
-    {
+	public partial class UsagePanel : Panel
+	{
 		#region Constructors
 		
-        // Called when created from unmanaged code
-        public UsagePanel(IntPtr handle) : base(handle)
-        {
-            Initialize();
-        }
+		// Called when created from unmanaged code
+		public UsagePanel (IntPtr handle) : base (handle)
+		{
+			Initialize ();
+		}
 		
-        // Called when created directly from a XIB file
-        [Export ("initWithCoder:")]
-        public UsagePanel(NSCoder coder) : base(coder)
-        {
-            Initialize();
-        }
+		// Called when created directly from a XIB file
+		[Export ("initWithCoder:")]
+		public UsagePanel (NSCoder coder) : base (coder)
+		{
+			Initialize ();
+		}
 		
-        // Shared initialization code
-        private void Initialize()
-        {
-        }
+		// Shared initialization code
+		void Initialize ()
+		{
+		}
 		
 		#endregion
-
-
-    }
+	}
 }
 
